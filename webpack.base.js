a6952b5fe4a429e99e4281a8bb56e001942eb69b
chenrenbin <2296006399@qq.com>
2019-12-04 12:09:51 +0800

    '多入口代码分离'

--- webpack.base.js
+++ webpack.base.js
@@ -3,9 +3,13 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: {
+    index: './src/index.js',
+    another: './src/print.js'
+  },
   output: {
     filename: '[name].bundle.js',
+    // chunkFilename: '[name].bundle.js', // 动态import的模块名
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/' // 用于webpack-dev-middleware+express
   },
@@ -15,5 +19,16 @@ module.exports = {
       filename: 'index.html', // 输出到dist的文件名，默认index.html
       template: 'index.html'  // 输出文件的模板，绝对/相对路径
     })
-  ]
+  ],
+  optimization: { // 代码分离
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: "commons",
+          chunks: "initial",
+          minChunks: 2
+        }
+      }
+    }
+  }
 };
